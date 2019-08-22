bf7f98a815bc2bb007ea57bc0fa083113428503d
Anh Bui <lf2foce@gmail.com>
2019-08-22 16:05:09 +0700

    styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :category
   has_rich_text :body
   
-  include PgSearch
+  include PgSearch::Model
     pg_search_scope :search_by_title, against: [:title, :content]
 end
