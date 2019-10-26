9d4105f0686452c6d540bc0bed8463705d5e5aa8
Anh Bui <lf2foce@gmail.com>
2019-10-26 22:57:36 +0700

    add hash_tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :category
   has_rich_text :body
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
+
+  after_commit :create_hash_tags, on: :create
+
+  def create_hash_tags
+	  extract_name_hash_tags.each do |name|
+	    hash_tags.create(name: name)
+	  end
+	end
+  def extract_name_hash_tags
+	  content.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+	end
   
   include PgSearch::Model
     pg_search_scope :search_by_title, against: [:title, :content]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_034943) do
+ActiveRecord::Schema.define(version: 2019_10_26_152227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,21 @@ ActiveRecord::Schema.define(version: 2019_08_22_034943) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "hash_tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_hash_tags", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "hash_tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hash_tag_id"], name: "index_post_hash_tags_on_hash_tag_id"
+    t.index ["post_id"], name: "index_post_hash_tags_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
