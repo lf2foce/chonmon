c2a9eb0492571095ed0834483ed70f478573161f
Anh Bui <lf2foce@gmail.com>
2019-10-31 15:37:07 +0700

    xx

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,15 @@ class Post < ApplicationRecord
   has_many :post_hash_tags
   has_many :hash_tags, through: :post_hash_tags
 
-  after_commit :create_hash_tags, on: :create
+  after_commit :create_hash_tags, on: [:create]
+  after_commit :update_hash_tags, on: [:update]
+
+  def update_hash_tags
+  	hash_tags.destroy_all
+	  extract_name_hash_tags.each do |name|
+	    hash_tags.create(name: name)
+	  end
+	end
 
   def create_hash_tags
 	  extract_name_hash_tags.each do |name|
