8efa8d21bec2f117a11f2495979494fda8d32719
Anh Bui <lf2foce@gmail.com>
2019-11-02 17:04:25 +0700

    redis to go heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,11 +37,11 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
+config.web_socket_server_url = "wss://chonmon66.herokuapp.com/cable" 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://chonmon66.herokuapp.com', 'http://chonmon66.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
