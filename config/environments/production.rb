42f003372807090f6209081deedceecc9aa1a58d
Anh Bui <lf2foce@gmail.com>
2019-11-02 17:23:07 +0700

    buildback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-config.web_socket_server_url = "wss://chonmon66.herokuapp.com/cable" 
+  config.web_socket_server_url = "wss://chonmon66.herokuapp.com/cable" 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
