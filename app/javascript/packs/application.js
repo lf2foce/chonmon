e51e130b595a7f07a4be4094f552c281cfc530f5
Anh Bui <lf2foce@gmail.com>
2019-11-02 17:47:03 +0700

    thu lai xem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,9 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 
-window.jQuery = $;
-window.$ = $;
+//window.jQuery = $;
+//window.$ = $;
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
